dd55c52330c3153e9626370438f86649a98ee3ce
lihorne <lihorne@uwaterloo.ca>
2014-04-07 07:23:11 -0700

    keydown not keypress

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@ var my_notes = localStorage.getItem("my_notes");
 
 $('#notes')[0].innerHTML = my_notes;
 
-$('#notes').keypress(function(event){
+$('#notes').keydown(function(event){
   localStorage.setItem("my_notes", $('#notes')[0].innerHTML)
 });
